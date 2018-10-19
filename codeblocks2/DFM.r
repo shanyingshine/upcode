6ed3b8df718a302d1a462fde9c70e69e739733df
shanyingshine <40597360+shanyingshine@users.noreply.github.com>
2018-10-19 16:50:46 +0800

    codeblocks2
    ultimate code

---
+++ codeblocks2/DFM.r
@@ -0,0 +1,30 @@
+library(MARSS)
+library(readxl)
+
+data1 <- read.csv("C:/Users/Administrator/desktop/codeblocks2/Processed_datasets/train_datasets.csv",header = T,check.names = F)
+row.names(data1) <- data1[,1]
+data1 <- data1[,-1]
+ncols <- ncol(data1)
+#print("please input the season you want to predict")
+#print("参照格式2018.1")
+#ty <-edit()
+#delta_s <- as.integer(((ty-floor(ty))-(as.numeric(colnames(data1)[1])-floor(as.numeric(colnames(data1)[1]))))*10)
+#delta_t <-as.integer(4*(floor(ty)-floor(as.numeric(colnames(data1)[1])))+delta_s)
+features <- data1[,1:delta_t]
+line1 <- rep(NA,nrow(features))
+features1 <- cbind(features,line1)
+features1 <- as.matrix(features1)
+#features1
+modd = MARSS(features1)
+modd1 = MARSS(y=features1,inits = modd$par)
+outt = augment.marssMLE(x=modd1,type.predict = c("observations", "states"), interval = "confidence", conf.level = 0.95)
+outt
+
+#feature_index <- read.csv('C:/Users/Administrator/desktop/codeblocks2/Processed_datasets/feature_index.csv',fileEncoding = "gbk",header = F,stringsAsFactors = F)
+#feature_index <- as.vector(unlist(feature_index[1]))
+indexf <- subset(outt, t==(delta_t+1))
+print(indexf)
+#index_fitted <- subset(outt, t==(delta_t+1) & .rownames %in% feature_index)$.fitted
+#print(index_fitted)
+write.csv(indexf , 'C:/Users/Administrator/desktop/codeblocks2/Processed_datasets/Index_fitted.csv')
+#write.table(t(index_fitted) , 'C:/Users/Administrator/iCloudDrive/Documents/code/upcode/codeblocks/Processed_datasets/Index_ch_fitted.txt',col.names = F,row.names = F,sep = ',')
---
+++ codeblocks2/Data_Preprocess.py
@@ -0,0 +1,82 @@
+# coding: utf-8
+import numpy as np
+import pandas as pd
+# In[1]:
+#如果数据没有列名，即年份，则
+#df_sheet = pd.read_csv('./index2.0.csv ',encoding = 'gbk',header = None,index_col = 0 )
+#df_sheet =df_sheet.T
+##行数是df_sheet.shape[0]，列数是df_sheet.shape[1]
+#a = ['2010','2011','2012','2013','2014','2015','2016','2017','2018']
+#b = ['.1','.2','.3','.4']
+#ab = []
+#for i in a:
+#    for j in b:
+#        s = i + j
+#        ab.append(s)
+#ab = list(map(float,ab))
+#ab=pd.DataFrame(ab)
+#for i in range(df_sheet.shape[1]+1,36):
+#    ab = ab.drop(i)
+#df_sheet = df_sheet.set_index(list(np.array(ab.T)))
+# In[2]
+#数据集录入,如果有列名
+#usecol = int(input("输入训练集个数:"))
+df_sheet = pd.read_excel('./Index0825.xlsx',sheet_name=0,encoding='utf8',header=0,index_col=0)
+df_sheet = df_sheet.T
+#df_sheet = df_sheet.drop(df_sheet.columns[32],axis =1)
+df_cols = df_sheet.shape[1]
+
+# In[3]
+train_data = df_sheet.copy()
+
+#检查是否有缺失值,并进行填补
+null_1,null_2 = np.where(train_data.isnull());np.where(train_data.isnull())
+ind=np.arange(1,len(null_1)+1)
+miss_matrix=np.array([null_1,null_2,ind])
+for i in ind:
+        train_data.iloc[miss_matrix[0,i-1],miss_matrix[1,i-1]] = (train_data.iloc[miss_matrix[0,i-1],miss_matrix[1,i-1]-1]+train_data.iloc[miss_matrix[0,i-1],miss_matrix[1,i-1]+1])/2
+
+train_data.to_csv('./Processed_datasets/train_datasets.csv',encoding="gbk")
+
+
+train_data = train_data.T
+#读入gdp数据
+gdp_data= pd.read_excel('./季度.xlsx',sheet_name=0,skiprows=29,encoding='utf8',header=None,index_col=0)
+gdp_cols = gdp_data.shape[1]
+if (df_cols-gdp_cols)!=0:
+    gdp_data = gdp_data.iloc[:,:(df_cols-gdp_cols)]
+gdp_data = gdp_data.T
+
+
+#合并指标数据与gdp数据
+gdp_data.index = train_data.index
+train_data_withGDP = pd.concat([train_data,gdp_data['地区生产总值']],axis=1)
+
+
+
+#读取三产数据
+sc_data = pd.read_csv("./sanchan.csv",encoding = "gbk",header = None,index_col = 0, nrows = 3)
+if (df_cols-gdp_cols)!=0:
+    sc_data = sc_data.iloc[:,64:(df_cols-gdp_cols)]
+else:
+    sc_data = sc_data.iloc[:,64:]
+sc_data = sc_data.T
+sc_data.index = train_data.index
+train_data_withSC = pd.concat([train_data,sc_data],axis=1)
+train_data_withSCGDP = pd.concat([train_data_withSC,gdp_data],axis =1)
+
+
+
+#数据集转置
+train_data = train_data.T
+gdp_data = gdp_data.T
+sc_data = sc_data.T
+train_data_withSC = train_data_withSC.T
+train_data_withGDP =train_data_withGDP.T
+train_data_withSCGDP = train_data_withSCGDP.T
+#输出gdp，三产和季度指标数据，第一列为指标
+sc_data.to_csv("./Processed_datasets/Sc_data.csv",encoding = 'gbk')
+gdp_data.to_csv("./Processed_datasets/Gdp_data.csv",encoding="gbk")
+train_data_withGDP.to_csv('./Processed_datasets/Datasets_WithGdp.csv',encoding="gbk")
+train_data_withSC.to_csv('./Processed_datasets/Datasets_WithSC.csv',encoding="gbk")
+train_data_withSCGDP.to_csv('./Processed_datasets/Datasets_WithSCGDP.csv',encoding="gbk")
